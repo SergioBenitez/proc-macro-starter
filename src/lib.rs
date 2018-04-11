e85997c19e5f9aa3bcd3ea5dc50a58e9135de555
Divyahans Gupta <divyahansg@gmail.com>
2018-04-11 11:46:18 -0700

    Handle tuple-like structs of length 1

--- src/lib.rs
+++ src/lib.rs
@@ -20,31 +20,30 @@ use syn::*;
 
 const NO_GENERICS: &str = "structs with generics cannot derive `UriDisplay`";
 const ONLY_STRUCTS: &str = "`UriDisplay` can only be derived for structs";
-const ONLY_NAMED: &str = "`UriDisplay` can only be derived for named-field structs";
-const EMPTY_STRUCT_WARN: &str = "deriving `UriDisplay` for empty struct";
+const NO_EMPTY_STRUCTS: &str = "`UriDisplay` cannot be derived for empty struct";
+const NO_UNIT_STRUCTS: &str = "`UriDisplay` can only be derived for unit structs";
+const ONLY_ONE_UNNAMED: &str = "`UriDisplay` can be derived for tuple-like structs of length only 1";
 
-fn validate_input(input: DeriveInput) -> PResult<FieldsNamed> {
-    // This derive doesn't support generics. Error out if there are generics.
-    if !input.generics.params.is_empty() {
-        return Err(input.generics.span().error(NO_GENERICS));
+fn validate_struct(data_struct: &DataStruct, input: &DeriveInput) -> PResult<()> {
+
+    let fields = &data_struct.fields;
+
+    match fields {
+        Fields::Named(fields_named) => {},
+        Fields::Unnamed(fields_unnamed) => {
+            if fields_unnamed.unnamed.len() > 1 {
+                return Err(input.span().error(ONLY_ONE_UNNAMED))
+            }
+        },
+        Fields::Unit => return Err(input.span().error(NO_UNIT_STRUCTS))
     }
 
-    // This derive only works for structs. Error out if the input is not a struct.
-    let input_span = input.span();
-    let data = input.data.into_struct().ok_or_else(|| input_span.error(ONLY_STRUCTS))?;
-
-    // This derive only works for named-field structs.
-    let named_fields = match data.fields {
-        Fields::Named(fields) => fields,
-        _ => return Err(input_span.error(ONLY_NAMED))
-    };
-
-    // Emit a warning if the struct is empty.
-    if named_fields.named.is_empty() {
-        Span::call_site().warning(EMPTY_STRUCT_WARN).emit();
+    // Reject empty structs.
+    if fields.is_empty() {
+        return Err(input.span().error(NO_EMPTY_STRUCTS))
     }
 
-    Ok(named_fields)
+    Ok(())
 }
 
 
@@ -54,19 +53,80 @@ fn real_derive_uri_display_value(input: TokenStream) -> PResult<TokenStream> {
         Span::call_site().error(format!("error: failed to parse input: {:?}", e))
     })?;
 
-    // Validate the struct.
+
+    // This derive doesn't support generics. Error out if there are generics.
+    if !input.generics.params.is_empty() {
+        return Err(input.generics.span().error(NO_GENERICS));
+    }
+
+    let inp = &input;
+
+    match inp.data {
+        Data::Struct(ref data_struct) => {
+            validate_struct(data_struct, &input)?;
+            real_derive_uri_display_value_for_struct(data_struct, &input)
+        },
+        _ => Err(input.span().error(ONLY_STRUCTS))
+    }
+}
+
+// Precondition: input must be valid non-unit struct
+fn real_derive_uri_display_value_for_struct(
+    data_struct: &DataStruct, input: &DeriveInput
+) -> PResult<TokenStream> {
+
+    let fields = &data_struct.fields;
+
+    match fields {
+        Fields::Named(fields_named) =>
+            real_derive_uri_display_value_for_named_struct(&fields_named, data_struct, input),
+        Fields::Unnamed(fields_unnamed) =>
+            real_derive_uri_display_value_for_unnamed_struct(&fields_unnamed, data_struct, input),
+        _ => panic!("This codepath is never reached.") // TODO: something better
+    }
+}
+
+// Precondition: there is exactly one field in the struct
+fn real_derive_uri_display_value_for_unnamed_struct(
+    fields_unnamed: &FieldsUnnamed, data_struct: &DataStruct, input: &DeriveInput
+) -> PResult<TokenStream> {
+
     let name = input.ident;
-    let struct_fields = validate_input(input)?;
+    let scope = Ident::from(format!("scope_{}", name.to_string().to_lowercase()));
+    Ok(quote! {
+        mod #scope {
+            extern crate std;
+            extern crate rocket;
+
+            use self::std::prelude::v1::*;
+            use self::std::fmt;
+            use self::rocket::http::uri::*;
+
+            impl UriDisplay for #name {
+                fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                  write!(f, "{}", &self.0 as &UriDisplay)
+                }
+            }
+        }
+    }.into())
+}
+
+fn real_derive_uri_display_value_for_named_struct(
+    fields_named: &FieldsNamed, data_struct: &DataStruct, input: &DeriveInput
+) -> PResult<TokenStream> {
 
     // Enumerate all the field names in the struct.
-    let idents = struct_fields.named.iter().map(|v| v.ident.as_ref().expect("named field"));
+    let idents = fields_named.named.iter().map(|v| v.ident.as_ref().expect("named field"));
     // Generate format string.
-    let format_string = struct_fields.named.iter().map(|v| v.ident.as_ref().unwrap().to_string() + "={}")
-                                                  .collect::<Vec<_>>()
-                                                  .join("&");
+    let format_string = fields_named.named.iter().map(|v| v.ident.as_ref().unwrap().to_string() + "={}")
+                                                 .collect::<Vec<_>>()
+                                                 .join("&");
+
+    let name = input.ident;
+    let scope = Ident::from(format!("scope_{}", name.to_string().to_lowercase()));
     // Generate the implementation.
     Ok(quote! {
-        mod scope {
+        mod #scope {
             extern crate std;
             extern crate rocket;
 
--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,16 @@ pub struct Person {
 	age: u8,
 }
 
+#[derive(UriDisplay)]
+pub struct Animal(Person);
+
 pub fn main() {
 	let p = Person { name: "john smith", age: 5 };
 	use rocket::http::uri::UriDisplay;
+	println!("{}", (&p as &UriDisplay).to_string());
 	assert_eq!((&p as &UriDisplay).to_string(), "name=john%20smith&age=5");
+
+	let q = Animal(p);
+	println!("{}", (&q as &UriDisplay).to_string());
+	assert_eq!((&q as &UriDisplay).to_string(), "name=john%20smith&age=5");
 }
